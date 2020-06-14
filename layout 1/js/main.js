9dba2602b6db1f25df27e2b879faca5c67f86310
Viacheslav <vyacheslav.seleznev87@gmail.com>
2020-06-14 11:34:56 +0900

    Это наш первый commit

---
+++ layout 1/js/main.js
@@ -0,0 +1,60 @@
+'use strict';
+
+
+// let persone = {
+//     name: "Сява",
+//     lastName: "Селезнев",
+//     old: 25
+// }
+
+// console.log(persone ["lastName"]);
+
+
+
+// let switcher = null;
+//     if (switcher) { 
+// console.log('Working..')
+// }
+
+// switcher = 1;
+//     if (switcher) { 
+// console.log('Working..')
+// }
+
+
+// alert('Hello World!')
+
+// let answer = confirm('Are you here');
+// console.log(answer);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[2]);
+
+// let answer = prompt('Are you 18 years old?', 'yes');
+// // console.log(answer);
+// console.log(typeof(arr));
+
+// console.log(3 + 4 * 5);
+
+// console.log(2 + 2 * 3);
+
+// console.log(1 + 2 * 2);
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// let isClosed = false;
+// let isClaver = true;
+// let isClean = false;
+
+// console.log(isClosed || isClaver || isClean);
+
+// console.log(a = b = 5);
+
+console.log(4 * 4 ** 2); // 4 * 9
